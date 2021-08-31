44a9921991e86e86fc6dfcea011ce68b21a95b2f
Shree <Shree@ADMINRG-GIBFHU5>
2021-08-31 18:53:37 +0530

    Add project files.

---
+++ Balance/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Balance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int balance = 10000, withdrawl;
+            Console.Write("Enter Withdwawl amount: ");
+            withdrawl = Convert.ToInt32(Console.ReadLine());
+            balance = withdrawl > balance ? balance : balance - withdrawl;
+            Console.WriteLine("Balance=" + balance);
+        }
+    }
+}
